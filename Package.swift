8a6a51ab25bcc8fded7ae36a456cea1d0b5460f9
Tristan Leblanc <tristan@moosefactory.eu>
2017-06-13 09:49:32 +0200

    Links against MooseFactory StORM version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2),
-        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1),
+        .Package(url: "https://github.com/moosefactory/StORM.git", majorVersion: 1),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
     ]
 )
